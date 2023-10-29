361a9e50a0ad4dff83b3c4e3df8bb49d170149dd
Nur-Asyl <n-kanagatov@mail.ru>
2023-10-29 20:43:00 +0600

    websocket con

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"server/db"
 	"server/internal/user"
+	"server/internal/ws"
 	"server/router"
 )
 
@@ -16,7 +17,10 @@ func main() {
 	userRep := user.NewRepository(dbConn.GetDB())
 	userSvc := user.NewService(userRep)
 	userHandler := user.NewHandler(userSvc)
+	hub := ws.NewHub()
+	wsHandler := ws.NewHandler(hub)
+	go hub.Run()
 
-	router.InitRouter(userHandler)
+	router.InitRouter(userHandler, wsHandler)
 	router.Start("0.0.0.0:8080")
 }
